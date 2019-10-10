b72e24c895ed59e7b96c29ca832d7f8036587523
Deep Gandhi <46297564+deep1401@users.noreply.github.com>
2019-10-10 22:02:38 +0530

    time hour done

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
+<<<<<<< HEAD
+from flask import Flask, request,jsonify
+from flask_cors import CORS,cross_origin
+import pandas as pd
+import numpy as np
+=======
 from flask import Flask, request
 from flask_cors import CORS,cross_origin
 import pandas as pd
-
+>>>>>>> 3b7d4246c80f07c57e908f5c015f1fb8d666e225
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 #load csv files
+
 recom=pd.read_csv('BPM2.csv')
-recom.set_index(recom.columns[0],inplace=True)
+recom.set_index(recom.columns[0], inplace=True)
 persuade=pd.read_csv('persuade2.csv')
+time=pd.read_csv('Time_hour.csv')
+time.set_index('time_hour', inplace=True)
+gb = time.groupby(['time_hour'])
+result = gb['category_id'].unique()
 
 
 def recommendation(customer_id):
@@ -16,6 +27,13 @@ def recommendation(customer_id):
     return recom.loc[customer_id].to_json()
 
 
+<<<<<<< HEAD
+def timehour(hour):
+    return {"hour": hour, "category_id": result.loc[hour].tolist()}
+
+
+=======
+>>>>>>> 3b7d4246c80f07c57e908f5c015f1fb8d666e225
 @app.route('/')
 def hello():
     return "Welcome: Please put /recom for getting recommendation predictions"
@@ -24,6 +42,11 @@ def hello():
 @app.route('/recom',methods=['POST'])
 def recommend():
     return recommendation(request.form.get('customer_id', type=int))
+
+
+@app.route('/hour',methods=['POST'])
+def hourtime():
+    return jsonify(timehour(request.form.get('hour', type=int)))
 
 
 if __name__ == '__main__':
