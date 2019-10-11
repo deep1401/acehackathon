3173c2f31bb28162ad481de371ce97a89ba80644
Deep Gandhi <46297564+deep1401@users.noreply.github.com>
2019-10-11 08:46:45 +0530

    fraud detection done

--- app.py
+++ app.py
@@ -15,10 +15,14 @@ CORS(app, support_credentials=True)
 recom=pd.read_csv('BPM2.csv')
 recom.set_index(recom.columns[0], inplace=True)
 persuade=pd.read_csv('persuade2.csv')
+persuade.columns=['index','customer_id']
 time=pd.read_csv('Time_hour.csv')
 time.set_index('time_hour', inplace=True)
 gb = time.groupby(['time_hour'])
 result = gb['category_id'].unique()
+fraud=pd.read_csv('fraud.csv')
+fraud.set_index('customer_id',inplace=True)
+
 
 
 def recommendation(customer_id):
@@ -26,10 +30,12 @@ def recommendation(customer_id):
     return recom.loc[customer_id].to_json()
 
 
-
 def timehour(hour):
     return {"hour": hour, "category_id": result.loc[hour].tolist()}
 
+
+def fraud1(customer_id):
+    return fraud.loc[customer_id].to_json()
 
 
 @app.route('/')
@@ -45,6 +51,16 @@ def recommend():
 @app.route('/hour',methods=['POST'])
 def hourtime():
     return jsonify(timehour(request.form.get('hour', type=int)))
+
+
+@app.route('/persuade',methods=['GET'])
+def pers():
+    return persuade.loc[:10].to_json()
+
+
+@app.route('/fraud',methods=['POST'])
+def freud():
+    return fraud1(request.form.get('customer_id', type=int))
 
 
 if __name__ == '__main__':
